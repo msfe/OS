aa7e2b6617c19925bafae48e0d5db69aaeaecb77
Mattias Folke <matfol@kth.se>
2014-05-01 12:38:43 +0200

    part two done

--- lab2.c
+++ lab2.c
@@ -8,13 +8,47 @@
 
 int main(int argc, char *argv[], char **envp)
 {
-  int MAX_STRING_LEN = 70; //given in the assigment
-  char input[MAX_STRING_LEN];
-  
-  printf("Welcome to miniTerm 0.1. Please state your commands\n");
-  printf(">");
-  scanf("%s", input);
+  // int MAX_STRING_LEN = 70; //given in the assigment
+  // char input[MAX_STRING_LEN];
 
-  char *args[] = {input, NULL}; /* The less-argument */
-  execvp(args[0], args);
+  // printf("Welcome to miniTerm 0.1. Please state your commands\n");
+  // printf(">");
+  // scanf("%s", input);
+
+  // char *args[] = {input, NULL}; /* The less-argument */
+  // execvp(args[0], args);
+
+  int status;
+  pid_t cpid;
+
+  cpid = fork(); /* Create a pipe */
+
+  /* If processID is invalid value */
+  if (cpid == -1) 
+  { 
+    perror("fork");
+    exit(EXIT_FAILURE);
+  }
+
+  /* Child */
+  if (cpid == 0) 
+  {    
+    printf("%s\n","I'm the Child!");
+    exit(EXIT_SUCCESS);
+  } 
+  /* Parent */
+  else 
+  { 
+    while(1){
+      waitpid(cpid, &status, 0);
+      if(status == 0){
+        break; //Terminated in a correct way
+      } else {
+        //Duno
+        // exit(EXIT_FAILURE);
+        //Do nothing
+      }
+    }
+    printf("%s\n","I'm the Parent!");
+  }
 }
