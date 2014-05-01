d66be34c8157159c69607a9995d7bd3ccdac8d77
Mattias Folke <matfol@kth.se>
2014-05-01 12:51:24 +0200

    part three done

--- lab2.c
+++ lab2.c
@@ -8,47 +8,54 @@
 
 int main(int argc, char *argv[], char **envp)
 {
-  // int MAX_STRING_LEN = 70; //given in the assigment
-  // char input[MAX_STRING_LEN];
-
-  // printf("Welcome to miniTerm 0.1. Please state your commands\n");
-  // printf(">");
-  // scanf("%s", input);
-
-  // char *args[] = {input, NULL}; /* The less-argument */
-  // execvp(args[0], args);
-
+  int MAX_STRING_LEN = 70; //given in the assigment
+  char input[MAX_STRING_LEN];
   int status;
   pid_t cpid;
 
-  cpid = fork(); /* Create a pipe */
+  char EXIT_STRING[] = "exit";
 
-  /* If processID is invalid value */
-  if (cpid == -1) 
-  { 
-    perror("fork");
-    exit(EXIT_FAILURE);
-  }
+  printf("Welcome to miniTerm 0.1. Please state your commands\n");
 
-  /* Child */
-  if (cpid == 0) 
-  {    
-    printf("%s\n","I'm the Child!");
-    exit(EXIT_SUCCESS);
-  } 
-  /* Parent */
-  else 
-  { 
-    while(1){
-      waitpid(cpid, &status, 0);
-      if(status == 0){
-        break; //Terminated in a correct way
-      } else {
+
+  while(1){
+    printf(">");
+    scanf("%s", input);
+    if (strcmp(input,EXIT_STRING) == 0){
+      exit(EXIT_SUCCESS);
+    }
+
+    cpid = fork(); /* Create a pipe */
+
+    /* If processID is invalid value */
+    if (cpid == -1) 
+    { 
+      perror("fork");
+      exit(EXIT_FAILURE);
+    }
+
+    /* Child */
+    if (cpid == 0) 
+    {    
+    char *args[] = {input, NULL}; /* execute the argument */
+      execvp(args[0], args);
+      exit(EXIT_SUCCESS);
+    } 
+    
+    /* Parent */
+    else 
+    { 
+      while(1){
+        waitpid(cpid, &status, 0);
+        if(status == 0){
+          break; //Terminated in a correct way
+        } else {
         //Duno
         // exit(EXIT_FAILURE);
         //Do nothing
+        }
       }
+      // printf("%s\n","I'm the Parent!");
     }
-    printf("%s\n","I'm the Parent!");
   }
 }
